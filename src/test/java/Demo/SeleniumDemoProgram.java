9a00c81b62bc5d3c7cfb08869ddde852be84fa50
swathi <ch.swathia409@gmail.com>
2023-12-14 07:58:20 +0530

    modified configaration

--- src/test/java/Demo/SeleniumDemoProgram.java
+++ src/test/java/Demo/SeleniumDemoProgram.java
@@ -4,19 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumDemoProgram {
 
-	@Parameters("Browsername")
+	@Parameters("Browser")
 	@Test
-	public void loginTest(String browser)
+	public void loginTest(String BrowserName)
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.orangehrm.com/");
-		System.out.println(browser);
+		System.out.println(BrowserName);
 		System.out.println(driver.getTitle());
 		driver.quit();
 	}
